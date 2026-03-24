85ba654279d102a1d004af4c463271fc8f6966db
VALLISHA ATLOOR SOMAYAGI <vas.atloor@gmail.com>
2026-03-24 15:34:06 +0100

    Update MainActivity.kt

--- android/app/src/main/java/com/grocerystore/MainActivity.kt
+++ android/app/src/main/java/com/grocerystore/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.grocerystore
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.grocerystore.ui.navigation.NavGraph
@@ -9,6 +11,12 @@ import com.grocerystore.ui.theme.GroceryStoreTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.setDefaultUncaughtExceptionHandler { _, e ->
+            Log.e("GroceryStore", "CRASH: ${e.message}", e)
+            runOnUiThread {
+                Toast.makeText(this, "Crash: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+        }
         setContent {
             GroceryStoreTheme {
                 NavGraph()
